06082032955f3c9eab1a350ed30c79d41cbe651f
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-02 17:44:34 +0530

    initial commit

--- Hiru_newbuild/lambda.js
+++ Hiru_newbuild/lambda.js
@@ -1,6 +1,22 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	
+	
+	
+	ddb.get({
+		TableName: 'ThuvvaTable',
+		Key: {
+			'ID': '001'
+		}
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
